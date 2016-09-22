0f8bfd65cea23be0b69851889f28eeaf1add7429
soltean <sergiu.c.oltean@gmail.com>
2016-09-22 11:30:52 +0300

    pitest

--- src/test/java/com/so/pitest/HumanTest.java
+++ src/test/java/com/so/pitest/HumanTest.java
@@ -9,14 +9,28 @@ public class HumanTest {
     @Test
     public void testHumanCapabilities() {
         Human human = new Human();
-        assertTrue("Humans must be normal", !human.canTravelWithTheSpeedOfLight(1));
+        assertTrue("Humans must be normal, cannot travel with the speed of light", !human.canTravelWithTheSpeedOfLight(1));
         assertTrue("Speedy Gonzales is not human", human.canTravelWithTheSpeedOfLight(299792458));
         assertTrue("Flash is not human", human.canTravelWithTheSpeedOfLight(999999999));
         assertTrue("Humans should not be able to lift planes", human.getNumberOfAirplanesHeCanLift() == 0);
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void testHumanCannotEatMoreThanHisLimit() {
+        Human human = new Human();
+        human.eatChocolate(15);
+    }
+
     @Test
-    public void testHumanNotFull() {
+    public void testHumanCanEatUnderHisLimit() {
+        Human human = new Human();
+        int number = 3;
+        human.eatChocolate(number);
+        assertTrue("Human not full", human.getNumberOfChocolateEatenToday() == number);
+    }
+
+    @Test
+    public void testHumanCanEatUntilHeIsFull() {
         Human human = new Human();
         int number = 3;
         human.eatChocolate(number);
@@ -26,25 +40,13 @@ public class HumanTest {
         assertTrue("Human full", human.getNumberOfChocolateEatenToday() == human.getNumberOfChocolatesCanEatPerDay());
     }
 
-    @Test
-    public void testHumanCanEat5Chocolates() {
-        Human human = new Human();
-        int number = 5;
-        human.eatChocolate(number);
-        assertTrue("Human not full", human.getNumberOfChocolateEatenToday() == number);
-    }
-
     @Test(expected = IllegalArgumentException.class)
-    public void testHumanFull() {
+    public void testHumanCannotEatMoreThanHisLimitCase2() {
         Human human = new Human();
         human.eatChocolate(5);
         assertTrue("Human full", human.getNumberOfChocolateEatenToday() == human.getNumberOfChocolatesCanEatPerDay());
         human.eatChocolate(5);
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testHumanFull2() {
-        Human human = new Human();
-        human.eatChocolate(15);
-    }
+
 }
